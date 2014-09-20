4ba9f017842892398ee54476476729ddbdd42d75
Martin Broerse <broerse@martinic.com>
2014-09-20 04:13:15 +0200

    Remove Pouch from route

--- app/routes/posts.js
+++ app/routes/posts.js
@@ -2,15 +2,5 @@ import Ember from "ember";
 export default Ember.Route.extend({
 	model: function() {
 		return this.get('store').find('post');
-	},
-  afterModel: function (recordArray) {
-    // This tells PouchDB to listen for live changes and
-    // notify Ember Data when a change comes in.
-    new PouchDB('bloggr').changes({
-      since: 'now',
-      live: true
-    }).on('change', function () {
-      recordArray.update();
-    });
-  }
+	}
 });
